211029edbae70b3c548e9e00ffa147e42da6bb32
Barry Leibson <bleibson@Vostro-230.nycap.rr.com>
2014-04-25 17:06:53 -0400

    Various improvements

--- vagrant/3.10/bootstrap.sh
+++ vagrant/3.10/bootstrap.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -f
 
+. /etc/profile.d/jdk.sh
 # parse args
 
 putQuickstart=0
@@ -22,39 +23,46 @@ if [ -e /etc/apache2/conf.d/phppgadmin ] ; then
   apachectl restart
 fi
 
-if [ ! $pureQuickstart ] ; then
+if [ $pureQuickstart ] ; then
+  echo "***bootstrap: Running in pure Quickstart mode."
+else
 
   # make sure you have the latest bw command before running it
 
+  echo "***bootstrap: Updating bw.sh"
   svn update --non-interactive --trust-server-cert $qs/bedework/build/quickstart/linux/bw.sh
 
-  # create blended config dir from default and jboss-postgresql.  Use Postgresql 
-  # for main database and set the bedework password to "xxx"
-
-  cd $qs/bedework/config/bwbuild
-  cp -pr jboss-postgresql vagrant
-  cp default/bedework*ds.xml vagrant
-  sed 's%<password></password>%<password>xxx</password>%' jboss-postgresql/bedework-ds.xml > vagrant/bedework-ds.xml
-  cp jboss-postgresql/cal.properties vagrant
-
-
-  # update and build as user "vagrant"
-
-  buildArgs="-bwchome $qs/bedework/config/bwbuild -bwc vagrant"
+  echo "***bootstrap: updating source and rebuilding"
+  buildArgs="-quickstart"
   su vagrant -c "cd $qs; ./bw -updateall; ./bw $buildArgs deploy; ./bw $buildArgs -tzsvr"
 fi
 
+# change default dialect for bedework dbase to Postgresql
+
+echo "***bootstrap: setting hibernate dialect to Postgresql"
+dbconfigFile=$qs/bedework/config/bedework/bwcore/dbconfig.xml
+sed 's/org.hibernate.dialect.HSQLDialect/org.hibernate.dialect.PostgreSQLDialect/' ${dbconfigFile}> ${dbconfigFile}.NEW
+cp $dbconfigFile ${dbconfigFile}.ORI
+mv ${dbconfigFile}.NEW $dbconfigFile
+
+echo "***bootstrap: installing databsource settings"
+dsSrcDir=$qs/bedework/config/datasources/postgresql
+sed 's%<password></password>%<password>xxx</password>%' $dsSrcDir/bedework-ds.xml > $jboss/server/default/bwdeploy/bedework-ds.xml
+
+echo "***bootstrap: downloading jdbc for Postgresql"
+cd $jboss/server/default/lib
+wget http://jdbc.postgresql.org/download/postgresql-9.3-1101.jdbc41.jar
 
 # deployConf
 
+echo "***bootstrap: deploying configuration"
 if [ ! $pureQuickstart ] ; then
   su vagrant -c "cd $qs; ./bw $buildArgs deployConf"
 else
   su vagrant -c "cd $qs; ./bw -quickstart deployConf"
 fi
 
-# set up start up logic
-
+echo "***bootstrap: installing start-up logic"
 cd /vagrant/data
 cp init.d.bedework /etc/init.d/bedework
 cp runbw.sh $qs
@@ -64,23 +72,29 @@ cp /tmp/run.sh $jboss/bin/run.sh
 chmod 755 /etc/init.d/bedework $qs/runbw.sh $jboss/bin/run.sh
 
 
-# start bedework
-
+echo "***bootstrap: starting up JBoss and ApacheDS"
 /etc/init.d/bedework start
 
 
-# wait for jmx-console to become available, then reindex and set up the postgresql db
-
+echo "***bootstrap: waiting for jmx-console to be available"
 wget -out /dev/null --retry-connrefused http://localhost:5080/jmx-console
-su vagrant -c "$jboss/bin/twiddle.sh invoke org.bedework.bwengine:service=indexing rebuildIndex"
 if [ ! $pureQuickstart ] ; then
-  su vagrant -c "$jboss/bin/twiddle.sh setattrs org.bedework.bwengine.core:service=DbConf HibernateDialect org.hibernate.dialect.PostgreSQLDialect Export True"
+  echo "***bootstrap: setting dumprestore attributes"
+  su vagrant -c "$jboss/bin/twiddle.sh setattrs org.bedework.bwengine.core:service=DbConf Export True SchemaOutFile $jboss/server/default/data/bedework/dumprestore/schema.txt"
+  echo "***bootstrap: exporting schema"
   su vagrant -c "$jboss/bin/twiddle.sh invoke org.bedework.bwengine.core:service=DbConf schema"
+  echo "***bootstrap: restoring data"
   su vagrant -c "$jboss/bin/twiddle.sh invoke org.bedework.bwengine:service=dumprestore restoreData"
 fi
+echo "***bootstrap: reindexing"
+su vagrant -c "$jboss/bin/twiddle.sh invoke org.bedework.bwengine:service=indexing rebuildIndex"
 
 # set up any conveniences
 
+echo "***bootstrap: setting up convenience links, etc."
 cd ~vagrant
 ln -s $jboss/server/default/log .
-chown vagrant log
+ln -s $qs/bedework/config/bwbuild .
+ln -s $qs qs
+ln -s $jboss jboss
+chown vagrant log bwbuild qs jboss
